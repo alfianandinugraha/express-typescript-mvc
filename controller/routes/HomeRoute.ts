7f97d692ef853b74c56a044284ef64d80a340c5a
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-07-01 21:06:15 +0700

    buat halaman untuk homepage

--- controller/routes/HomeRoute.ts
+++ controller/routes/HomeRoute.ts
@@ -3,7 +3,9 @@ import { Request, Response } from 'express';
 export default class HomeRoute {
     public get() {
         return (req: Request, res: Response) => {
-            res.send(`<h1>Hello homepage !</h1>`);
+            res.render('pages/home', {
+                titlePage: 'Homepage'
+            })
         };
     }
 }
